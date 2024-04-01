c5dacf17b3fae1b2443fe9f696917546a9e12f57
VadimR89 <136196738+VadimR89@users.noreply.github.com>
2024-04-01 19:54:04 +0300

    Add files via upload

---
+++ src/entity/models.py
@@ -0,0 +1,39 @@
+import enum
+from datetime import date
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, func, Enum
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Contact(Base):
+    __tablename__ = 'contacts'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50), index=True)
+    last_name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(50))
+    phone_number: Mapped[str] = mapped_column(String(12))
+    born_date: Mapped[str] = mapped_column(String(20))
+    completed: Mapped[bool] = mapped_column(default=False)
+
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
+    role: Mapped[Enum] = mapped_column('role', Enum(Role), default=Role.user, nullable=True)
---
+++ src/repository/contacts.py
@@ -0,0 +1,72 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.entity.models import Contact
+from src.schemas.contact import ContactSchema, ContactUpdateSchema
+
+
+async def get_contacts(limit: int, offset: int, db: AsyncSession):
+    stmt = select(Contact).offset(offset).limit(limit)
+    contacts = await db.execute(stmt)
+    return contacts.scalars().all()
+
+
+async def get_contact(contact_id: int, db: AsyncSession):
+    stmt = select(Contact).filter_by(id=contact_id)
+    contact = await db.execute(stmt)
+    return contact.scalar_one_or_none()
+
+
+async def get_contact_by_params(
+        db: AsyncSession,
+        first_name: str = None,
+        last_name: str = None,
+        email: str = None
+):
+    query = select(Contact)
+
+    if first_name:
+        query = query.filter(Contact.first_name == first_name)
+    if last_name:
+        query = query.filter(Contact.last_name == last_name)
+    if email:
+        query = query.filter(Contact.email == email)
+
+    contact = await db.execute(query)
+    return contact.scalar_one_or_none()
+
+
+async def create_contact(body: ContactSchema, db: AsyncSession):
+    contact = Contact(**body.model_dump(exclude_unset=True))
+    db.add(contact)
+    await db.commit()
+    await db.refresh(contact)
+    return contact
+
+
+async def update_contact(contact_id: int, body: ContactUpdateSchema, db: AsyncSession):
+    stmt = select(Contact).filter_by(id=contact_id)
+    result = await db.execute(stmt)
+    contact = result.scalar_one_or_none()
+    if contact:
+        contact.first_name = body.first_name
+        contact.last_name = body.last_name
+        contact.email = body.email
+        contact.phone_number = body.phone_number
+        contact.born_date = body.born_date
+        contact.completed = body.completed
+        await db.commit()
+        await db.refresh(contact)
+    return contact
+
+
+async def delete_contact(contact_id: int, db: AsyncSession):
+    stmt = select(Contact).filter_by(id=contact_id)
+    contact = await db.execute(stmt)
+    contact = contact.scalar_one_or_none()
+    if contact:
+        await db.delete(contact)
+        await db.commit()
+    return contact
+
+
+
