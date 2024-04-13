533e9c82f7792c90a2f06f6690d933ab802b6e25
VadimR89 <136196738+VadimR89@users.noreply.github.com>
2024-04-13 19:38:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI, Depends, HTTPException
+from ipaddress import ip_address
+from typing import Callable
+import re
+
+import redis.asyncio as redis
+from fastapi import FastAPI, Depends, HTTPException, Request, status
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi_limiter import FastAPILimiter
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.db import get_db
-from src.routes import contacts
-from src.routes import auth
+from src.routes import contacts, auth, users
+from src.conf.config import config
 
 app = FastAPI()
-
+banned_ips = [ip_address("192.168.1.1"), ip_address("192.168.1.2")]
+user_agent_ban_list = []
 origins = ["*"]
 
 app.add_middleware(
@@ -18,14 +28,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth.router, prefix="/api")
-app.include_router(contacts.router, prefix="/api")
 
+@app.middleware("http")
+async def user_agent_ban_middleware(request: Request, call_next: Callable):
+    user_agent = request.headers.get("user-agent")
+    for ban_pattern in user_agent_ban_list:
+        if re.search(ban_pattern, user_agent):
+            return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"detail": "You are banned"})
+    response = await call_next(request)
+    return response
+
+
+app.mount("/static", StaticFiles(directory='src/static'), name='static')
+app.include_router(auth.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
+app.include_router(contacts.router, prefix="/api")
 
 
 @app.get("/")
 def index():
     return {"message": "Contacts Application"}
+
+
+@app.on_event("startup")
+async def startup():
+    r = await redis.Redis(host=config.REDIS_DOMAIN, port=config.REDIS_PORT, db=0, password=config.REDIS_PASSWORD)
+    await FastAPILimiter.init(r)
+
+
+templates = Jinja2Templates(directory='src/templates')
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {"request": request})
 
 
 @app.get("/api/healthchecker")
