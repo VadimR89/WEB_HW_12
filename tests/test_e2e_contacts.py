5541ea747a32438b6f79c5d8c17009bacb374d6d
VadimR89 <136196738+VadimR89@users.noreply.github.com>
2024-04-22 19:30:51 +0300

    Update test_e2e_contacts.py

--- tests/test_e2e_contacts.py
+++ tests/test_e2e_contacts.py
@@ -4,6 +4,18 @@ from src.services.auth import auth_service
 
 
 def test_get_contacts(client, get_token):
+    """
+The test_get_contacts function tests the GET /api/contacts endpoint.
+It does so by first mocking out the redis cache, and then making a request to
+the endpoint with an Authorization header containing a valid JWT token. The
+response is checked for status code 200 (OK), and then its JSON data is checked
+to ensure that it contains no contacts.
+
+:param client: Make requests to the flask app
+:param get_token: Get a valid token for the test
+:return: An empty list
+:doc-author: Trelent
+"""
     with patch.object(auth_service, "cache") as redis_mock:
         redis_mock.get.return_value = None
         token = get_token
@@ -15,6 +27,19 @@ def test_get_contacts(client, get_token):
 
 
 def test_create_contact(client, get_token, monkeypatch):
+    """
+The test_create_contact function tests the creation of a contact.
+    It does this by first patching the auth_service's cache object to return None, which is what it would do if there was no token in the cache.
+    Then, it creates a token using get_token and stores that in headers as an authorization header.
+    Next, it sends a POST request to /api/contacts with headers containing our authorization header and json data containing title: test and description: test.
+    Finally, we assert that response status code is 201 (created), then we store response data into variable 'data'. We assert that id exists
+
+:param client: Make requests to the api
+:param get_token: Get a valid token for the test
+:param monkeypatch: Patch the cache object in auth_service
+:return: A 201 status code and the contact data
+:doc-author: Trelent
+"""
     with patch.object(auth_service, "cache") as redis_mock:
         redis_mock.get.return_value = None
         token = get_token
