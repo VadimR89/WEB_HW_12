fc783a57547f078b06507f96a9c03da5a3fc8bd8
VadimR89 <136196738+VadimR89@users.noreply.github.com>
2024-04-22 19:30:03 +0300

    Update test_e2e_users.py

--- tests/test_e2e_users.py
+++ tests/test_e2e_users.py
@@ -4,6 +4,19 @@ from src.services.auth import auth_service
 
 
 def test_get_me(client, get_token, monkeypatch):
+    """
+The test_get_me function tests the /api/users/me endpoint.
+It does so by first patching the auth_service's cache with a mock redis object, and then setting up mocks for
+the FastAPILimiter class' redis, identifier, and http_callback methods. It then creates a token using get_token
+and adds it to an Authorization header before sending a GET request to /api/users/me. Finally it asserts that the
+response status code is 200.
+
+:param client: Make requests to the api
+:param get_token: Get the token from the fixture
+:param monkeypatch: Mock the redis, identifier and http_callback functions
+:return: The user's information
+:doc-author: Trelent
+"""    
     with patch.object(auth_service, "cache") as redis_mock:
         redis_mock.get.return_value = None
         monkeypatch.setattr("fastapi_limiter.FastAPILimiter.redis", AsyncMock())
